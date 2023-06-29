1122f2aa4a0245611732c85202343422d0e159e3
张展业 <lucaszhangzy@aliyun.com>
2023-06-29 14:36:56 +0800

    SpringSecurityOAuth2第三方登录(github)初体验示例代码

---
+++ oauth2-third-part-login/demo1/src/main/java/com/example/demo1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello World";
+    }
+}
